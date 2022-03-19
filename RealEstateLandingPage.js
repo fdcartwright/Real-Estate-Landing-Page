981c426f3eef83e363d2adfea8b8a24883883eb1
Dan Cartwright <f.d.cartwright@hotmail.com>
2022-03-19 14:00:49 -0700

    added pages and html structure

---
+++ RealEstateLandingPage.js
@@ -0,0 +1,15 @@
+
+// Your project must include 4 of the 6 following features (but may include more):
+// One or more Classes (must use static methods and/or prototype methods)
+// Sets, updates, or changes local storage
+// Contains form fields, validates those fields
+
+// 1. Validate forms and address
+
+// 2. google API fetch requeest to validate address
+
+// 3. Timing function to show banner at top of the page for special - fade in/fade out.
+
+// 4. Jasmine unit test
+
+
